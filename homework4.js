c78aac21a7fa2e43ad4ff10edc9b6747ccc9b040
tobuw <tobuwidjaja@gmail.com>
2022-07-11 23:46:30 -0400

    Homework 4

--- homework4.js
+++ homework4.js
@@ -20,35 +20,32 @@
  * print grade C
  */
  
-
-let studentScore = 42; 
+let studentScore = 1; 
 let maxScore = 50; 
 let percentage = (studentScore/maxScore) * 100;
 
-if(studentScore > maxScore){
+if(studentScore > maxScore || percentage < 0){
     console.log('Invalid student score')
 }
-else if(percentage > 91 && percentage < 100){
+else if(percentage >= 91 && percentage <= 100){
     console.log('Grade: A');
 }
-else if (percentage > 81 && percentage < 90.99){
+else if (percentage >= 81 && percentage <= 90.99){
     console.log('Grade B');
 }
-else if (percentage > 71 && 80.99){
+else if (percentage >= 71 && percentage <= 80.99){
     console.log('Grade C');
 }
-else if (percentage > 61 && 70.99){
+else if (percentage >= 61 && percentage <= 70.99){
     console.log('Grade D');
 }
-else if (percentage > 51 && 60.99){
+else if (percentage >= 51 && percentage <= 60.99){
     console.log('Grade E');
 }
-else if (percentage < 51 && percentage > 0){
+else {
     console.log('Grade F')
 }
-else{
-    console.log('Invalid student score')
-}
+
 
 /*
 Create a varialbe and store any value in it 
@@ -60,16 +57,16 @@ if the number is NOT divisible by 5 and by 3, print the value in myNumber
 
 let storedNumber = 13; 
 if(storedNumber % 5 === 0 && storedNumber % 3 === 0){
-    console.log('Divisible by 5 and 3')
+    console.log('\nDivisible by 5 and 3')
 }
 else if(storedNumber % 5 === 0){
-    console.log('Divisible by 5');
+    console.log('\nDivisible by 5');
 }
 else if (storedNumber % 3 === 0){
-    console.log('Divisible by 3');
+    console.log('\nDivisible by 3');
 }
 else{
-    console.log(storedNumber);
+    console.log(`\n${storedNumber}`);
 }
 
 /*
@@ -83,11 +80,11 @@ let planets = ['Earth','Mercury', 'Jupiter', 'Saturn', 'Mars', 'Venus', 'Pluto']
     console.log('Earth is mentioned in expected index')
 }*/
 if(planets[0] === 'Earth'){
-    console.log('Earth is mentioned in expected index')
+    console.log('\nEarth is mentioned in expected index')
 }
 else{
     planets.unshift('Earth');
-    console.log(`New planets array is: ${planets}`);
+    console.log(`\nNew planets array is: ${planets}`);
 }
 
 /*
@@ -97,18 +94,17 @@ if BASKETBALL is present in index-2, print 'BASKETBALL is present at index-2'
 if BASKETBALL Is NOT present in the array, replace index-2 value with BASKETBALL and print the value which you replaced.
 */
 
-const sports = ['Football', 'Soccer', 'Baseball', 'Rugby']
+const sports = ['Football', 'Soccer', 'BASKETBALL', 'Baseball', 'Rugby']
 
-
-if(sports[2] === 'basketball'.toUpperCase()){
-    console.log('Basketball is present at index 2')
+if(sports[2] === 'BASKETBALL'){
+    console.log('\nBasketball is present at index 2')
 }
-else if((sports.includes('basketball'.toUpperCase())) === true){
-    console.log('Basketball is mentioned in the sports array')
+else if((sports.includes('BASKETBALL')) === true){
+    console.log('\nBasketball is mentioned in the sports array')
 }
 else{
-    console.log('The replaced value is: ' + sports.splice(2, 1, 'basketball'.toUpperCase()));
-    console.log(`New array: ${sports}`);
+    console.log('\nThe replaced value is: ' + sports.splice(2, 1, 'BASKETBALL'));
+    //console.log(`New array: ${sports}`);
 }
 
 
